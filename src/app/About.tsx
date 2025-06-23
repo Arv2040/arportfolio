8ffd8b23cca8810699709acf84cc199ef3999424
Arv2040 <aravcr4085@gmail.com>
2025-06-23 20:43:41 +0530

    feat:fixed landing text side and padding of about

--- src/app/About.tsx
+++ src/app/About.tsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <section className="w-full h-full  md:w-[50em] md:h-[25em] bg-[#161B22] border-2 border-[#58A6FF] flex flex-col justify-center items-center text-[#C9D1D9] mt-10 rounded-xl">
-      <p className="text-center text-md sm:p-2 md:p-4 md:text-2xl leading-relaxed">
+      <p className="text-center text-md p-2 md:p-4 md:text-2xl leading-relaxed">
         Hey, I’m Arav — just a regular guy who loves building things and occasionally yelling at the TV during Manchester United matches. I enjoy trying out new ideas, whether it’s a tech stack or a random hobby. When something clicks, I give it my full focus and try to do it really well. I like to keep things simple, learn as I go, and improve bit by bit. The dream? To be the best at what I do — one bug fix, one goal celebration, and one late-night coding session at a time.
       </p>
     </section>
--- src/app/home.tsx
+++ src/app/home.tsx
@@ -3,7 +3,7 @@ import Typewriter from 'typewriter-effect';
 export default function Home() {
   return (
     <section className="w-full h-[20em] md:w-[50em] md:h-[25em] bg-[#161B22] border-2 border-[#58A6FF] flex flex-col gap-y-2 justify-center items-center text-white mt-10 rounded-xl">
-      <p className="text-[#58A6FF] w-full text-center text-3xl sm:p-2 md:p-4 md:text-6xl font-bold">
+      <p className="text-[#58A6FF] w-full text-center text-2xl sm:p-2 md:p-4 md:text-6xl font-bold">
         Hello! I am Arav Jain
       </p>
       <div className="text-[#C9D1D9] w-full text-center text-2xl sm:p-2 md:p-4 md:text-4xl">
